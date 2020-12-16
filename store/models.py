efc048bca8be8af8a0142e84f025efc99ff8b5cc
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-16 20:24:22 +0100

    add a get_absolute_url() method to the Category model

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
@@ -12,6 +13,9 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',args=[self.slug])
 
 
 class Product(models.Model):
@@ -29,3 +33,4 @@ class Product(models.Model):
         index_together = (('id', 'slug'),)
     def __str__(self):
         return self.name
+
