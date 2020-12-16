d38303de625b0ac5d0a87a8ca521558397b5e2b7
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-16 20:26:30 +0100

    add a get_absolute_url() method to the Product model

--- store/models.py
+++ store/models.py
@@ -34,3 +34,6 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('store:product_detail',args=[self.id, self.slug])
+
