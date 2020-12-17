ee7a4d691e62352e22aebffa47c0c4ff9eb9bdfb
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-17 17:03:41 +0100

    include the cart urls in main urls.py

--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls', namespace='store')),
+    path('cart/', include('cart.urls', namespace='cart')),
 ]
 
 if settings.DEBUG:
