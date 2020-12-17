203266efb6dab291eb49ceb31a99f2e01f361de7
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-17 13:08:35 +0100

    update installed apps

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store',
+    'store.apps.StoreConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
