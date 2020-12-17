c4d9e5f3c9dca62f4480698ba5ec5c3b071b3025
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-17 11:13:16 +0100

    retrieve and display a single product by adding small logic for product detail method

--- store/views.py
+++ store/views.py
@@ -8,7 +8,6 @@ def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
-    print(category_slug)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
@@ -18,4 +17,5 @@ def product_list(request, category_slug=None):
     'products': products})
 
 def product_detail(request, id, slug):
-    pass
+    product = get_object_or_404(Product,id=id,slug=slug,available=True)
+    return render(request,'shop/product/detail.html',{'product': product})
