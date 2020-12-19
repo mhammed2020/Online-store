a108d2c2e85be39cf13c0471db7bcefbed2fdc7a
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-19 18:57:22 +0100

    create first view of the payment application

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
+import braintree
+from django.shortcuts import render, redirect, get_object_or_404
+from django.conf import settings
+from orders.models import Order
+# instantiate Braintree payment gateway
+gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
+def payment_process(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+    total_cost = order.get_total_cost()
+    if request.method == 'POST':
