9ace6d77a3f0cb69c61dc4f2779274881fe4193b
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-20 15:22:43 +0100

    edit admin.py for orders app

--- orders/admin.py
+++ orders/admin.py
@@ -38,15 +38,15 @@ export_to_csv.short_description = 'Export to CSV'
 
 
 def order_detail(obj):
-url = reverse('orders:admin_order_detail', args=[obj.id])
-return mark_safe(f'<a href="{url}">View</a>')
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
     'address', 'postal_code', 'city', 'paid',
-    'created', 'updated']
+    'created', 'updated',order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
