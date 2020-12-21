15cdb52446a18051673feb7b8d26dc17b54b050e
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-21 16:39:12 +0100

    Applying coupons to order model

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from store.models import Product
+
+from decimal import Decimal
+from django.core.validators import MinValueValidator, \
+MaxValueValidator
+from coupons.models import Coupon
+
+
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -11,6 +18,10 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     braintree_id = models.CharField(max_length=150, blank=True)
+
+
+    coupon = models.ForeignKey(Coupon,related_name='orders',null=True,blank=True,on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0,validators=[MinValueValidator(0), MaxValueValidator(100)])
     class Meta:
         ordering = ('-created',)
     def __str__(self):
