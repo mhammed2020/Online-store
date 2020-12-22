bb9be8d8ed9eda9cd74d225797a8a8c48d72132a
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-22 21:23:58 +0100

    edit Product model

--- store/models.py
+++ store/models.py
@@ -18,7 +18,7 @@ unique=True)
 )
 
     class Meta:
-        ordering = ('name',)
+        # ordering = ('name',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
     def __str__(self):
@@ -29,27 +29,26 @@ unique=True)
 
 
 class Product(models.Model):
-    # category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
     # name = models.CharField(max_length=200, db_index=True)
     # slug = models.SlugField(max_length=200, db_index=True)
+    #     description = models.TextField(blank=True)
+
 
     translations = TranslatedFields(
-name = models.CharField(max_length=200, db_index=True),
-slug = models.SlugField(max_length=200, db_index=True),
-description = models.TextField(blank=True)
-)
+                    name = models.CharField(max_length=200, db_index=True),
+                    slug = models.SlugField(max_length=200, db_index=True),
+                    description = models.TextField(blank=True)
+                    )
 
-
-
+    category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
-    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    class Meta:
-        ordering = ('name',)
-        index_together = (('id', 'slug'),)
+    # class Meta:
+    #     ordering = ('name',)
+    #     index_together = (('id', 'slug'),)
     def __str__(self):
         return self.name
 
