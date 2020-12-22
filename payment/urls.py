0db2f85b542a21a37b8177f813ccad9d63b55506
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-22 12:30:01 +0100

    Translating URL patterns  for  paymenet and orders app "

--- payment/urls.py
+++ payment/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.utils.translation import gettext_lazy as _
 app_name = 'payment'
 urlpatterns = [
 path('process/', views.payment_process, name='process'),
