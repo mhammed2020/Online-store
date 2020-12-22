0fcaf49555e354340dc27587df4611008051c827
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-22 12:28:21 +0100

    Translating URL patterns

--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.conf.urls.i18n import i18n_patterns
+
+from django.utils.translation import gettext_lazy as _
+
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
         path('cart/', include('cart.urls', namespace='cart')),
