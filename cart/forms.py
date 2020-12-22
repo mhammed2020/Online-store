9580aab6252a10be2f24e5513f18997c3107b747
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-22 11:11:47 +0100

    fix TypeError: __init__() got an unexpected keyword argument

--- cart/forms.py
+++ cart/forms.py
@@ -4,5 +4,5 @@ from django.utils.translation import gettext_lazy as _
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,coerce=int,abel=_('Quantity'))
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,coerce=int,label=_('Quantity'))
     override = forms.BooleanField(required=False,initial=False,widget=forms.HiddenInput)
