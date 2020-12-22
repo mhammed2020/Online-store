08ba195ea264234e4db83735e302426c96b3121b
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-22 12:26:14 +0100

    Adding a language prefix to URL patterns

--- project/urls.py
+++ project/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-urlpatterns = [
+
+from django.conf.urls.i18n import i18n_patterns
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
         path('cart/', include('cart.urls', namespace='cart')),
 path('orders/', include('orders.urls', namespace='orders')),
@@ -25,7 +27,7 @@ path('payment/', include('payment.urls', namespace='payment')),
 path('coupons/', include('coupons.urls', namespace='coupons')),
 path('rosetta/', include('rosetta.urls')),
     path('', include('store.urls', namespace='store')),
-]
+)
 
 
 
