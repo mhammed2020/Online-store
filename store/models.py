4ed922b9180dd22c1ea56833f025cfdad6a7a65e
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-22 21:15:23 +0100

    Edit the Product model

--- store/models.py
+++ store/models.py
@@ -29,9 +29,18 @@ unique=True)
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True)
+    # category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
+    # name = models.CharField(max_length=200, db_index=True)
+    # slug = models.SlugField(max_length=200, db_index=True)
+
+    translations = TranslatedFields(
+name = models.CharField(max_length=200, db_index=True),
+slug = models.SlugField(max_length=200, db_index=True),
+description = models.TextField(blank=True)
+)
+
+
+
     image = models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
