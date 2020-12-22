2eb66971944063ed4d92df5de7de198fcb3b46cd
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-22 21:11:55 +0100

    add PARLER_LANGUAGES dictionay to settings

--- project/settings.py
+++ project/settings.py
@@ -169,3 +169,14 @@ LOCALE_PATHS = (
 os.path.join(BASE_DIR, 'locale/'),
 )
 
+
+PARLER_LANGUAGES = {
+None: (
+{'code': 'en'},
+{'code': 'es'},
+),
+'default': {
+'fallback': 'en',
+'hide_untranslated': False,
+}
+}
