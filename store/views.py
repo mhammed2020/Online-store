9ab0b8878d03c01e49c55ca742941a74a89efafd
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-12-23 12:56:56 +0100

    Edit the views.py file of the store application

--- store/views.py
+++ store/views.py
@@ -10,7 +10,13 @@ def product_list(request, category_slug=None):
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
     if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
+        # category = get_object_or_404(Category, slug=category_slug)
+
+        language = request.LANGUAGE_CODE
+        category = get_object_or_404(Category,
+                                        translations__language_code=language,
+                                        translations__slug=category_slug)
+
         products = products.filter(category=category)
     return render(request,'store/product/list.html',
     {'category': category,
